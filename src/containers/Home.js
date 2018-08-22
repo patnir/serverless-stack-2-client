6989d886531525a0b2c3f00a13c3a95236860084
Rahul Patni <rpatni@purdue.edu>
2018-08-22 19:24:56 +0530

    Committing a typo

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -36,29 +36,31 @@ export default class Home extends Component {
   handleNoteClick = event => {
     event.preventDefault();
     this.props.history.push(event.currentTarget.getAttribute("href"));
-  }
+  };
 
   renderNotesList(notes) {
     return [{}].concat(notes).map(
       (note, i) =>
-        i !== 0
-          ? <ListGroupItem
-              key={note.noteId}
-              href={`/notes/${note.noteId}`}
-              onClick={this.handleNoteClick}
-              header={note.content.trim().split("\n")[0]}
-            >
-              {"Created: " + new Date(note.createdAt).toLocaleString()}
-            </ListGroupItem>
-          : <ListGroupItem
-              key="new"
-              href="/notes/new"
-              onClick={this.handleNoteClick}
-            >
-              <h4>
-                <b>{"\uFF0B"}</b> Create a new note
-              </h4>
-            </ListGroupItem>
+        i !== 0 ? (
+          <ListGroupItem
+            key={note.noteId}
+            href={`/notes/${note.noteId}`}
+            onClick={this.handleNoteClick}
+            header={note.content.trim().split("\n")[0]}
+          >
+            {"Created: " + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        ) : (
+          <ListGroupItem
+            key="new"
+            href="/notes/new"
+            onClick={this.handleNoteClick}
+          >
+            <h4>
+              <b>{"\uFF0B"}</b> Create a new note
+            </h4>
+          </ListGroupItem>
+        )
     );
   }
 
@@ -66,7 +68,7 @@ export default class Home extends Component {
     return (
       <div className="lander">
         <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+        <p>A very expensive note taking app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
             Login
