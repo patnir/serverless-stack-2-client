13cfe50cde0c9a9690a7d36f9a98e87a23bb1b49
Rahul Patni <rpatni@purdue.edu>
2018-08-22 18:33:25 +0530

    updated config with prod and dev, added settings page, added stripe public keys to config, Adding a billing form, Connecting the billing form

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-16u0o1sx3jzwb"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://gnwarm90a7.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
-  }
+    USER_POOL_ID: "us-east-1_heZ8JkRXN",
+    APP_CLIENT_ID: "4s8tgk6kj3n2t9qmvotk722iqn",
+    IDENTITY_POOL_ID: "us-east-1:c06c6e2b-6f35-4061-8e8a-27581ed9ccdb"
+  },
+  STRIPE_KEY: "pk_test_WohyPOuZdk3nrhUBnnyAbMnW"
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-94abpypq4m8r"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://n8lu4tpmsg.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_MG2laLsMX",
+    APP_CLIENT_ID: "5e2otqcp38acnrjmb4gadhbbf2",
+    IDENTITY_POOL_ID: "us-east-1:164617ce-a612-4e04-a710-fa44f25a2850"
+  },
+  STRIPE_KEY: "pk_test_WohyPOuZdk3nrhUBnnyAbMnW"
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
